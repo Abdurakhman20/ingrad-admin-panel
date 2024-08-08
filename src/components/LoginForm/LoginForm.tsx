8eb566219eb425bae5c72681ff97a4dea03c1750
= <ausmanov706@gmail.com>
2024-08-08 17:24:05 +0300

    add show \ close password button

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { ILoginFormValues } from "../../pages/Login/Login";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import styles from "./LoginForm.module.css";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
@@ -18,6 +19,12 @@ const LoginForm: React.FC<ILoginFormProps> = () => {
     // Дальше данные можно закинуть в глобальное состояние
     console.log(data);
     reset();
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword((prev) => !prev);
   };
 
   return (
@@ -39,18 +46,27 @@ const LoginForm: React.FC<ILoginFormProps> = () => {
           <ErrorMessage message={errors.email?.message} />
         )}
 
-        <input
-          className={`${styles.form_field} ${styles.input}`}
-          type={"password"}
-          placeholder={"Пароль"}
-          {...register("password", {
-            required: "Пароль обязателен",
-            minLength: {
-              value: 4,
-              message: "Пароль должен содержать минимум 4 символов",
-            },
-          })}
-        />
+        <div className={styles.passwordContainer}>
+          <input
+            className={`${styles.form_field} ${styles.input} ${styles.passwordInput}`}
+            type={showPassword ? "text" : "password"} // Меняем тип на text или password
+            placeholder={"Пароль"}
+            {...register("password", {
+              required: "Пароль обязателен",
+              minLength: {
+                value: 4,
+                message: "Пароль должен содержать минимум 4 символов",
+              },
+            })}
+          />
+          <button
+            type="button"
+            className={styles.togglePassword}
+            onClick={toggleShowPassword}
+          >
+            {showPassword ? <IoMdEye size={20} /> : <IoMdEyeOff size={20} />}
+          </button>
+        </div>
         {errors.password?.message && (
           <ErrorMessage message={errors.password?.message} />
         )}
--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { IRegisterFormValues } from "../../pages/Register/Register";
 import styles from "./RegisterForm.module.css";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 interface IAuthFormProps {}
 
@@ -19,6 +20,17 @@ const RegisterForm: React.FC<IAuthFormProps> = () => {
     // Дальше данные можно закинуть в глобальное состояние
     console.log(data);
     reset();
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordAgain, setShowPasswordAgain] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const toggleShowPasswordAgain = () => {
+    setShowPasswordAgain((prev) => !prev);
   };
 
   return (
@@ -59,35 +71,60 @@ const RegisterForm: React.FC<IAuthFormProps> = () => {
         {errors.email?.message && (
           <ErrorMessage message={errors.email?.message} />
         )}
-        <input
-          className={`${styles.form_field} ${styles.input}`}
-          type={"password"}
-          placeholder={"Пароль"}
-          {...register("password", {
-            required: "Пароль обязателен",
-            minLength: {
-              value: 4,
-              message: "Пароль должен содержать минимум 4 символов",
-            },
-          })}
-        />
+        <div className={styles.passwordContainer}>
+          <input
+            className={`${styles.form_field} ${styles.input} ${styles.passwordInput}`}
+            type={showPassword ? "text" : "password"}
+            placeholder={"Пароль"}
+            {...register("password", {
+              required: "Пароль обязателен",
+              minLength: {
+                value: 4,
+                message: "Пароль должен содержать минимум 4 символов",
+              },
+            })}
+          />
+          <button
+            type="button"
+            className={styles.togglePassword}
+            onClick={toggleShowPassword}
+          >
+            {showPassword ? <IoMdEye size={20} /> : <IoMdEyeOff size={20} />}
+          </button>
+        </div>
+
         {errors.password?.message && (
           <ErrorMessage message={errors.password?.message} />
         )}
-        <input
-          className={`${styles.form_field} ${styles.input}`}
-          type={"password"}
-          placeholder={"Повторите пароль"}
-          {...register("passwordAgain", {
-            required: "Повторение пароля обязательно",
-            minLength: {
-              value: 4,
-              message: "Пароль должен содержать минимум 4 символов",
-            },
-            validate: (value) =>
-              value === getValues("password") || "Пароли не совпадают!",
-          })}
-        />
+
+        <div className={styles.passwordContainer}>
+          <input
+            className={`${styles.form_field} ${styles.input} ${styles.passwordInput}`}
+            type={showPasswordAgain ? "text" : "password"}
+            placeholder={"Повторите пароль"}
+            {...register("passwordAgain", {
+              required: "Повторение пароля обязательно",
+              minLength: {
+                value: 4,
+                message: "Пароль должен содержать минимум 4 символов",
+              },
+              validate: (value) =>
+                value === getValues("password") || "Пароли не совпадают!",
+            })}
+          />
+          <button
+            type="button"
+            className={styles.togglePassword}
+            onClick={toggleShowPasswordAgain}
+          >
+            {showPasswordAgain ? (
+              <IoMdEye size={20} />
+            ) : (
+              <IoMdEyeOff size={20} />
+            )}
+          </button>
+        </div>
+
         {errors.passwordAgain?.message && (
           <ErrorMessage message={errors.passwordAgain?.message} />
         )}
