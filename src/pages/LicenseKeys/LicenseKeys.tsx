6c7165571465c5cb9c1a2bc26c33a20029436dbe
= <ausmanov706@gmail.com>
2024-08-09 16:08:47 +0300

    create license keys page

--- src/pages/LicenseKeys/LicenseKeys.tsx
+++ src/pages/LicenseKeys/LicenseKeys.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "./LicenseKeys.module.css";
 
 const LicenseKeys: React.FC = () => {
-  return <div className="wrapper">License Keys</div>;
+  const handleLicenseDelete = () => {
+    const confirm: boolean = window.confirm("Вы действительно хотите удалить сессию?");
+
+    if (confirm) {
+      console.log("accepted!");
+    }
+  };
+  return (
+    <div className='wrapper'>
+      <h2 className={styles.title}>Лицензии</h2>
+      <div className={styles.licenseKeys_wrapper}>
+        <div className={styles.license}>
+          <div className={styles.license_content}>
+            <div>ID Лицензии: 1</div>
+            <div>Лицензия: C85929A2-89D8-4334-8537-7F53A7664D3F</div>
+            <div>Название: ingp</div>
+          </div>
+          <button className={styles.license_delete_btn} onClick={handleLicenseDelete}>
+            Удалить
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LicenseKeys;
