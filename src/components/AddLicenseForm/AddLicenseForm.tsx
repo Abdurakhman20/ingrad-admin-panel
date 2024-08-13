ff6b344be047298287b8add535332360e3d04aa7
= <ausmanov706@gmail.com>
2024-08-13 10:17:56 +0300

    add validation to license form

--- src/components/AddLicenseForm/AddLicenseForm.tsx
+++ src/components/AddLicenseForm/AddLicenseForm.tsx
@@ -1,15 +1,49 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./AddLicenseForm.module.css";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
+interface IAddLicenseValues {
+  name: string;
+  key: string;
+}
 
 const AddLicenseForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid, errors },
+  } = useForm<IAddLicenseValues>({ mode: "onChange" });
+
+  const onSubmit: SubmitHandler<IAddLicenseValues> = data => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className={styles.form_container}>
-      <form className={styles.license_form} onSubmit={event => event.preventDefault()}>
-        <input className={`${styles.form_field} ${styles.input}`} type={"text"} placeholder={"Название лицензии"} />
-        <input className={`${styles.form_field} ${styles.input}`} type={"text"} placeholder={"Ключ лицензии"} />
-        <button className={`${styles.form_field} ${styles.button}`} type="submit">
-          Добавить
-        </button>
+      <form className={styles.license_form} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className={`${styles.form_field} ${styles.input}`}
+          type={"text"}
+          placeholder={"Название лицензии"}
+          {...register("name", { required: "Пожалуйста введите название лицензии!" })}
+        />
+        {errors.name?.message && <ErrorMessage message={errors.name?.message} />}
+        <input
+          className={`${styles.form_field} ${styles.input}`}
+          type={"text"}
+          placeholder={"Ключ лицензии"}
+          {...register("key", { required: "Пожалуйста введите ключ лицензии!" })}
+        />
+        {errors.key?.message && <ErrorMessage message={errors.key?.message} />}
+
+        {isValid && (
+          <button className={`${styles.form_field} ${styles.button}`} type="submit">
+            Добавить
+          </button>
+        )}
       </form>
     </div>
   );
