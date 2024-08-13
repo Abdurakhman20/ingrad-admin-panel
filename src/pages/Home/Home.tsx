5ddd4c2ad9d55df4b0d09ee8fb6d01b50f87995d
= <ausmanov706@gmail.com>
2024-08-13 16:15:50 +0300

    add home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./Home.module.css";
+import RvtImg from "../../assets/revit_icon.jpg";
 
 const Home: React.FC = () => {
-  return <div className="wrapper">Home</div>;
+  return (
+    <div className="wrapper">
+      <h1 className={styles.title}>Админ-панель для сервера лицензий Revit</h1>
+      <div className={styles.img}>
+        <img alt="revit_img" src={RvtImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
