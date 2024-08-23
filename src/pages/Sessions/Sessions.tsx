1861f2110788e59dc3a910a77a1bbb9ccc9dfef8
= <ausmanov706@gmail.com>
2024-08-23 08:41:48 +0300

    use session data from backend

--- src/pages/Sessions/Sessions.tsx
+++ src/pages/Sessions/Sessions.tsx
@@ -6,60 +6,6 @@ import { ISession } from "../../models/ISession";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { fetchSessions, removeSession, SessionStatus } from "../../store/slices/sessionSlice";
 
-// Пока использую статические данные, так-как проблемы с backend
-const sessions_data = [
-  {
-    id: 1,
-    startingDateTime: "2024-08-06T08:56:21.2694613",
-    ipAddress: "::ffff:172.27.0.1",
-    windowsUserName: "ING0070_158",
-    revitUserName: "112",
-    revitVersion: "2022",
-    licenseKey: "84EC95C6-7572-4AB1-BCF5-5C04F1A63A58",
-    accessToken: "D40513BA-7B40-41B9-9450-785398C11EA5",
-  },
-  {
-    id: 2,
-    startingDateTime: "2024-08-06T06:56:21.2694613",
-    ipAddress: "::ffff:172.27.0.1",
-    windowsUserName: "ING0070_158",
-    revitUserName: "112",
-    revitVersion: "2022",
-    licenseKey: "84EC95C6-7572-4AB1-BCF5-5C04F1A63A58",
-    accessToken: "D40513BA-7B40-41B9-9450-785398C11EA5",
-  },
-  {
-    id: 3,
-    startingDateTime: "2024-08-06T06:56:21.2694613",
-    ipAddress: "::ffff:172.27.0.1",
-    windowsUserName: "ING0070_158",
-    revitUserName: "112",
-    revitVersion: "2022",
-    licenseKey: "84EC95C6-7572-4AB1-BCF5-5C04F1A63A58",
-    accessToken: "D40513BA-7B40-41B9-9450-785398C11EA5",
-  },
-  {
-    id: 4,
-    startingDateTime: "2024-08-06T06:56:21.2694613",
-    ipAddress: "::ffff:172.27.0.1",
-    windowsUserName: "ING0070_158",
-    revitUserName: "112",
-    revitVersion: "2022",
-    licenseKey: "84EC95C6-7572-4AB1-BCF5-5C04F1A63A58",
-    accessToken: "D40513BA-7B40-41B9-9450-785398C11EA5",
-  },
-  {
-    id: 5,
-    startingDateTime: "2024-08-06T06:56:21.2694613",
-    ipAddress: "::ffff:172.27.0.1",
-    windowsUserName: "ING0070_158",
-    revitUserName: "112",
-    revitVersion: "2022",
-    licenseKey: "84EC95C6-7572-4AB1-BCF5-5C04F1A63A58",
-    accessToken: "D40513BA-7B40-41B9-9450-785398C11EA5",
-  },
-];
-
 const Sessions: React.FC = () => {
   const dispatch = useAppDispatch();
   const sessions = useAppSelector(state => state.session.sessions);
@@ -126,8 +72,6 @@ const Sessions: React.FC = () => {
     },
   ];
 
-  console.log(sessions);
-
   return (
     <div className={styles.sessions}>
       <div className="wrapper">
@@ -135,7 +79,7 @@ const Sessions: React.FC = () => {
         {status === SessionStatus.LOADING ? (
           <p>Загрузка...</p>
         ) : (
-          <Table columns={columns} dataSource={sessions_data} className={styles.table} size="small" rowKey="id" />
+          <Table columns={columns} dataSource={sessions} className={styles.table} size="small" rowKey="id" />
         )}
       </div>
     </div>
